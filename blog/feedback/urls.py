b81ee1999a8bdf3b7dcdf49617866ad10dd40f61
Alex Santat <santat88@gmail.com>
2024-11-25 22:50:23 +0300

    Finished

--- blog/feedback/urls.py
+++ blog/feedback/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from .views import feedback_page
+from .views import feedback_page, add_feedback_page, thanks_page
 
 urlpatterns = [
-    path('', feedback_page)
+    path('', feedback_page, name = 'feedback_list'),
+    path('add_feedback/', add_feedback_page, name = 'add_feedback_record'),
+    path('thanks/', thanks_page)
 ]
--- blog/feedback/views.py
+++ blog/feedback/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from .models import feedbackRecord
 
 MENU = {'Главная':'/','Страница поста':'/post','О блоге':'/about', 'Отзывы':'/feedback'}
 
 def feedback_page(request):
+    feedbackRecords = feedbackRecord.objects.all()
     title = 'Cтраница с отзывами'
-    data = {"menu":MENU,"title":title}
+    data = {"menu":MENU,"title":title,"feedbackRecords":feedbackRecords}
     return render(request, './feedback.html', context=data)
+    
+    
+def add_feedback_page(request):
+    title = 'Добавить отзыв'
+    data = {"menu":MENU,"title":title}
+    return render(request, './add_feedback.html', context=data)
+    
+def thanks_page(request):
+    Name = request.POST['name']
+    Email = request.POST['email']
+    Text = request.POST['text']
+    feedbackRecord.objects.create(name = Name, email = Email, text = Text)
+    title = 'Спасибо за Ваш отзыв!'
+    data = {"menu":MENU,"title":title}
+    return render(request, './thanks.html', context=data)
